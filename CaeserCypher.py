889322ac1f3674e30eabf0c8c530624f42ac29be
DanielEJones <126533025+DanielEJones@users.noreply.github.com>
2023-02-28 18:55:54 +0800

    Now case-sensitive!

--- CaeserCypher.py
+++ CaeserCypher.py
@@ -1,8 +1,21 @@
-def translate(text, offset) -> str:
-    num = [(ord(c.lower()) - 97 + offset) % 26 + 97 if not c == " " else 32 for c in text]
-    return "".join([chr(n) for n in num])
+ASCII_UPPER_A = 65
+ASCII_LOWER_Z = 122
+ASCII_LOWER_A = 97
 
 
-text = "Esp njmpcnspq td nzzvtyr fa l dezcx"
-for i in range(26):
-    print(translate(text, i) + '!')
+def cipher(text, offset) -> str:
+    # old method:
+    # num = [(ord(c.lower()) - 97 + offset) % 26 + 97 if 97 <= ord(c.lower()) <= 122 else ord(c) for c in text]
+    # return ''.join([chr(n) for n in num])
+
+    def shift(char) -> str:
+        if ASCII_LOWER_A <= ord(char.lower()) <= ASCII_LOWER_Z:
+            start = ASCII_LOWER_A if char.islower() else ASCII_UPPER_A
+            char = chr((ord(char) - start + offset) % 26 + start)
+        return char
+    return ''.join([shift(char) for char in text])
+
+
+t = "What? Text that you will never be able to read? 1mp0551bl3!"
+o = 15
+print(f"The text '{t}' was translated by {o} characters to produce '{cipher(t, o)}'.")
